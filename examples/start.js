376e5513ad698e4c4851afd734fffac8e4ee1acf
mientjan <mientjan.stelling@gmail.com>
2014-10-06 11:50:32 +0200

    convert to AMD

---
+++ examples/start.js
@@ -0,0 +1,23 @@
+define(["require", "exports", '../src/easel/display/Stage', '../src/easel/display/Shape'], function(require, exports, Stage, Shape) {
+    //
+    require.config({
+        baseUrl: "../src/"
+    });
+
+    var Start = (function () {
+        function Start() {
+            var stage = new Stage(document.getElementById('canvasElement'));
+
+            var shape = new Shape();
+            shape.graphics.beginFill('#FF0000');
+            shape.graphics.drawRect(0, 0, 50, 50);
+
+            stage.addChild(shape);
+
+            stage.update();
+        }
+        return Start;
+    })();
+
+    var s = new Start();
+});
---
+++ examples/start.ts
@@ -0,0 +1,25 @@
+declare var require:any;
+//
+require.config( {
+	baseUrl: "../src/"
+});
+
+
+import Stage = require('../src/easel/display/Stage');
+import Shape = require('../src/easel/display/Shape')
+
+class Start {
+	constructor(){
+		var stage = new Stage( document.getElementById('canvasElement') );
+
+		var shape = new Shape();
+		shape.graphics.beginFill('#FF0000');
+		shape.graphics.drawRect(0,0,50,50);
+
+		stage.addChild(shape);
+
+		stage.update();
+	}
+}
+
+var s = new Start();
