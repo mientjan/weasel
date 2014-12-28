6bf0c69aac52a7d655be89c34c7159c3fbbc5bce
Mient - jan Stelling <mientjan.stelling@gmail.com>
2014-12-28 20:51:43 +0100

    some changes added extra webgl stuff

--- src/easelts/event/MouseEvent.js
+++ src/easelts/event/MouseEvent.js
@@ -4,7 +4,7 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-define(["require", "exports", '../../createts/events/Event'], function (require, exports, Event) {
+define(["require", "exports"], function (require, exports) {
     /*
      * MouseEvent
      * Visit http://createjs.com/ for documentation, updates and examples.
