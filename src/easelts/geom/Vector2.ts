98a251ae39e70c78dbecddd4e23353fe39d5efe3
Mient - jan Stelling <mient-jan@mediamonks.com>
2014-12-28 17:18:39 +0100

    replacing Point with Vector2 class

--- src/easelts/geom/Vector2.ts
+++ src/easelts/geom/Vector2.ts
@@ -1,8 +1,5 @@
 /*
- * Point
- * Visit http://createjs.com/ for documentation, updates and examples.
- *
- * Copyright (c) 2010 gskinner.com, inc.
+ * Vector2
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -33,6 +30,14 @@ import NumberUtil = require('./../util/NumberUtil');
  * @module easelts
  */
 
+/**
+ * @class Vector2
+ * @author mrdoob / http://mrdoob.com/
+ * @author philogb / http://blog.thejit.org/
+ * @author egraether / http://egraether.com/
+ * @author zz85 / http://www.lab4games.net/zz85/blog
+ * @author mient-jan stelling
+ */
 class Vector2
 {
 	/**
@@ -47,7 +52,431 @@ class Vector2
 	 * @type Number
 	 **/
 
-	constructor(public x:number, public y:number){}
+	constructor(public x:number, public y:number)
+	{
+	}
+
+	public set(x:number, y:number)
+	{
+
+		this.x = x;
+		this.y = y;
+
+		return this;
+	}
+
+	public setX(x:number):Vector2
+	{
+		this.x = x;
+		return this;
+	}
+
+	public setY(y:number):Vector2
+	{
+		this.y = y;
+		return this;
+	}
+
+	public setComponent(index:number, value:number):void
+	{
+		switch(index)
+		{
+			case 0:
+				this.x = value;
+				break;
+			case 1:
+				this.y = value;
+				break;
+			default:
+				throw new Error('index is out of range: ' + index);
+
+		}
+	}
+
+	public getComponent(index:number):number
+	{
+		switch(index)
+		{
+			case 0:
+				return this.x;
+			case 1:
+				return this.y;
+			default:
+				throw new Error('index is out of range: ' + index);
+
+		}
+	}
+
+	public copy(v:Vector2):Vector2
+	{
+
+		this.x = v.x;
+		this.y = v.y;
+
+		return this;
+
+	}
+
+	public add(v:Vector2):Vector2
+	{
+
+		this.x += v.x;
+		this.y += v.y;
+
+		return this;
+
+	}
+
+	public addVectors(a:Vector2, b:Vector2):Vector2
+	{
+		this.x = a.x + b.x;
+		this.y = a.y + b.y;
+
+		return this;
+	}
+
+	public addScalar(s:number):Vector2
+	{
+
+		this.x += s;
+		this.y += s;
+
+		return this;
+
+	}
+
+	public sub(v:Vector2)
+	{
+		this.x -= v.x;
+		this.y -= v.y;
+
+		return this;
+
+	}
+
+	public subVectors(a:Vector2, b:Vector2)
+	{
+
+		this.x = a.x - b.x;
+		this.y = a.y - b.y;
+
+		return this;
+
+	}
+
+	public multiply(v:Vector2)
+	{
+
+		this.x *= v.x;
+		this.y *= v.y;
+
+		return this;
+
+	}
+
+	public multiplyScalar(s:number)
+	{
+
+		this.x *= s;
+		this.y *= s;
+
+		return this;
+
+	}
+
+	public divide(v:Vector2)
+	{
+
+		this.x /= v.x;
+		this.y /= v.y;
+
+		return this;
+
+	}
+
+	public divideScalar(scalar:number)
+	{
+
+		if(scalar !== 0)
+		{
+
+			var invScalar = 1 / scalar;
+
+			this.x *= invScalar;
+			this.y *= invScalar;
+
+		}
+		else
+		{
+
+			this.x = 0;
+			this.y = 0;
+
+		}
+
+		return this;
+	}
+
+	public    min(v:Vector2)
+	{
+
+		if(this.x > v.x)
+		{
+
+			this.x = v.x;
+
+		}
+
+		if(this.y > v.y)
+		{
+
+			this.y = v.y;
+
+		}
+
+		return this;
+
+	}
+
+	public max(v:Vector2)
+	{
+
+		if(this.x < v.x)
+		{
+
+			this.x = v.x;
+
+		}
+
+		if(this.y < v.y)
+		{
+
+			this.y = v.y;
+
+		}
+
+		return this;
+
+	}
+
+	public clamp(min:Vector2, max:Vector2)
+	{
+
+		// This function assumes min < max, if this assumption isn't true it will not operate correctly
+
+		if(this.x < min.x)
+		{
+
+			this.x = min.x;
+
+		}
+		else if(this.x > max.x)
+		{
+
+			this.x = max.x;
+
+		}
+
+		if(this.y < min.y)
+		{
+
+			this.y = min.y;
+
+		}
+		else if(this.y > max.y)
+		{
+
+			this.y = max.y;
+
+		}
+
+		return this;
+	}
+
+	private __min:Vector2;
+	private __max:Vector2;
+
+	public clampScalar(minVal:number, maxVal:number)
+	{
+
+		if(this.__min === void 0)
+		{
+
+			this.__min = new Vector2(0, 0);
+			this.__max = new Vector2(0, 0);
+
+		}
+
+		this.__min.set(minVal, minVal);
+		this.__max.set(maxVal, maxVal);
+
+		return this.clamp(this.__min, this.__max);
+
+	}
+
+	public floor():Vector2
+	{
+		this.x = Math.floor(this.x);
+		this.y = Math.floor(this.y);
+
+		return this;
+	}
+
+	public ceil():Vector2
+	{
+
+		this.x = Math.ceil(this.x);
+		this.y = Math.ceil(this.y);
+
+		return this;
+
+	}
+
+	public round():Vector2
+	{
+
+		this.x = Math.round(this.x);
+		this.y = Math.round(this.y);
+
+		return this;
+
+	}
+
+	public roundToZero():Vector2
+	{
+
+		this.x = ( this.x < 0 ) ? Math.ceil(this.x) : Math.floor(this.x);
+		this.y = ( this.y < 0 ) ? Math.ceil(this.y) : Math.floor(this.y);
+
+		return this;
+
+	}
+
+	public negate():Vector2
+	{
+
+		this.x = -this.x;
+		this.y = -this.y;
+
+		return this;
+
+	}
+
+	public dot(v):number
+	{
+
+		return this.x * v.x + this.y * v.y;
+
+	}
+
+	public lengthSq():number
+	{
+
+		return this.x * this.x + this.y * this.y;
+
+	}
+
+	public length():number
+	{
+
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+
+	}
+
+	public normalize():Vector2
+	{
+
+		return this.divideScalar(this.length());
+
+	}
+
+	public distanceTo(v:Vector2):number
+	{
+
+		return Math.sqrt(this.distanceToSquared(v));
+
+	}
+
+	public distanceToSquared(v:Vector2):number
+	{
+
+		var dx = this.x - v.x, dy = this.y - v.y;
+		return dx * dx + dy * dy;
+
+	}
+
+	public setLength(l):Vector2
+	{
+
+		var oldLength = this.length();
+
+		if(oldLength !== 0 && l !== oldLength)
+		{
+
+			this.multiplyScalar(l / oldLength);
+		}
+
+		return this;
+
+	}
+
+	public lerp(v:Vector2, alpha:number):Vector2
+	{
+
+		this.x += ( v.x - this.x ) * alpha;
+		this.y += ( v.y - this.y ) * alpha;
+
+		return this;
+
+	}
+
+	public equals(v:Vector2):boolean
+	{
+
+		return ( ( v.x === this.x ) && ( v.y === this.y ) );
+
+	}
+
+	public fromArray(array:number[], offset:number):Vector2
+	{
+
+		if(offset === undefined)
+		{
+			offset = 0;
+		}
+
+		this.x = array[ offset ];
+		this.y = array[ offset + 1 ];
+
+		return this;
+	}
+
+	public toArray(array:number[], offset:number):number[]
+	{
+
+		if(array === undefined)
+		{
+			array = [];
+		}
+		if(offset === undefined)
+		{
+			offset = 0;
+		}
+
+		array[ offset ] = this.x;
+		array[ offset + 1 ] = this.y;
+
+		return array;
+
+	}
+
+	public clone():Vector2
+	{
+
+		return new Vector2(this.x, this.y);
+
+	}
+
 }
 
 export = Vector2;
