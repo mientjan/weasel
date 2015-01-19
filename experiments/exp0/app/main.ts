10913d353db8856bb7dc430c47424d144151a3e9
Mient - jan Stelling <mientjan.stelling@gmail.com>
2015-01-19 19:22:18 +0100

    adding some experiments

---
+++ experiments/exp0/app/main.ts
@@ -0,0 +1,38 @@
+import Shader = require('../../lib/Shader');
+
+function main(id){
+	// Get A WebGL context
+	var canvas = <HTMLCanvasElement> document.getElementById("canvas");
+	var gl = canvas.getContext("experimental-webgl");
+
+
+	var vertexShader = Shader.get(gl, "2d-vertex-shader");
+	var fragmentShader = Shader.get(gl, "2d-fragment-shader");
+	var program = Shader.program(gl, vertexShader, fragmentShader);
+	gl.useProgram(program);
+
+// look up where the vertex data needs to go.
+	var positionLocation = gl.getAttribLocation(program, "a_position");
+
+// Create a buffer and put a single clipspace rectangle in
+// it (2 triangles)
+	var buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.bufferData(
+		gl.ARRAY_BUFFER,
+		new Float32Array([
+			-1.0, -1.0,
+			1.0, -1.0,
+			-1.0,  1.0,
+			-1.0,  1.0,
+			1.0, -1.0,
+			1.0,  1.0]),
+		gl.STATIC_DRAW);
+	gl.enableVertexAttribArray(positionLocation);
+	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+// draw
+	gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+main('canvas');
