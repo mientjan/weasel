96119040c27d07789db4e1d8211617684d9ebdf0
Mient - jan Stelling <mientjan.stelling@gmail.com>
2015-02-28 19:41:31 +0100

    added ImageSequence

--- src/easelts/display/component/ImageSequence.ts
+++ src/easelts/display/component/ImageSequence.ts
@@ -8,7 +8,6 @@ import SignalConnection = require('../../../createts/event/SignalConnection');
  */
 class ImageSequence extends Bitmap
 {
-
 	public _playing = false;
 	public _timeIndex:number = -1;
 	public _frame:number = -1;
@@ -30,16 +29,13 @@ class ImageSequence extends Bitmap
 	 * @param {string|number} regX
 	 * @param {string|number} regY
 	 */
-	constructor(images:string[], fps:number, width:any, height:any, x:any = 0, y:any = 0, regX:any = 0, regY:any = 0)
+	constructor(images:HTMLImageElement[], fps:number, width:any, height:any, x:any = 0, y:any = 0, regX:any = 0, regY:any = 0)
 	{
 		super(images[0], width, height, x, y, regX, regY);
 
 		for(var i = 0; i < images.length; i++)
 		{
-			var img = document.createElement('img');
-			img.src = images[i];
-			this._images.push(img);
-
+			this._images.push( images[i] );
 		}
 
 		this._fps = 1000 / fps;
@@ -55,10 +51,10 @@ class ImageSequence extends Bitmap
 
 	public play(times = 1, onComplete:Function = null)
 	{
-		this._playing = true;
 		this._frame = 0;
 		this._times = times;
 		this._onComplete = onComplete;
+		this._playing = true;
 	}
 
 	public stop()
@@ -77,7 +73,6 @@ class ImageSequence extends Bitmap
 	public onTick(e:TimeEvent)
 	{
 		var playing = this._playing;
-
 
 		if(playing)
 		{
@@ -105,6 +100,9 @@ class ImageSequence extends Bitmap
 				{
 					this._frame = frame;
 					this.image = this._images[frame];
+
+					console.log(this._frame);
+					
 				}
 			}
 
@@ -113,4 +111,4 @@ class ImageSequence extends Bitmap
 
 }
 
-export  = ImageSequence;
+export = ImageSequence;
