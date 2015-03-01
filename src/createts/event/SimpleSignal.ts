1067e6ea663027c746a56f6cfe8a6ab6906bfa48
Mient - jan Stelling <mientjan.stelling@gmail.com>
2015-03-01 11:54:34 +0100

    Adding SimpleSignal

---
+++ src/createts/event/SimpleSignal.ts
@@ -0,0 +1,49 @@
+interface IGenerator {
+	():ISimpleSignal;
+
+	TYPE_REMOVE:number;
+	TYPE_REMOVE_ALL:number;
+}
+
+interface ISimpleSignal {
+	(callback:Function, remove:number):number
+}
+
+var Generator = <IGenerator> function()
+{
+	var _events = [];
+	var SimpleSignal = <ISimpleSignal> function(callback:Function = null, remove:number = 0){
+		var items:number = 0;
+
+		// emit
+		if(callback == void 0){
+			for(var i = 0; i < _events.length; i++)
+			{
+				_events[i].call(void 0);
+				items++;
+			}
+		} else {
+			if( !remove ){
+				_events.push(callback);
+				items++;
+			} else {
+				for(var i = 0; i < _events.length; i++)
+				{
+					if( _events[i] == callback ){
+						_events.splice(i, 1);
+						items++;
+					}
+
+				}
+			}
+		}
+
+		return items;
+	}
+}
+
+Generator.TYPE_REMOVE = 1;
+Generator.TYPE_REMOVE_ALL = 2;
+
+
+export = Generator;
