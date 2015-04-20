fa0ce4b426860bfc7e4a061d40b69041c046089d
Mient-jan Stelling <mientjan.stelling@gmail.com>
2015-04-20 15:55:36 +0200

    adding grunt taskmanager for future testing tasks and initial compile test to test if code does not have any superfisial errors

---
+++ tools/tasks/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+
+	grunt.initConfig({
+		ts: {
+			options:{
+				comments: false,
+				compile: true,
+				module: 'amd',
+				target: 'es5',
+				sourceMap: false
+
+			},
+			default : {
+
+				src: [
+					"!../../**/webgl.d.ts","../../src/**/*.ts"
+]
+}
+}
+});
+grunt.loadNpmTasks("grunt-ts");
+grunt.registerTask("default", ["ts"]);
+};
