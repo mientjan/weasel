0e4a5c85ddc1515dcd3fd112ffcd4872902274fa
Mient-jan Stelling <mientjan.stelling@gmail.com>
2015-05-07 12:13:35 +0200

    added RGBA toString method

--- src/easelts/data/RGBA.ts
+++ src/easelts/data/RGBA.ts
@@ -1,9 +1,15 @@
 class RGBA
 {
-	public r:number = 0.0;
-	public g:number = 0.0;
-	public b:number = 0.0;
-	public a:number = 0.0;
+	constructor(public r:number = 0.0, public g:number = 0.0, public b:number = 0.0, public a:number = 0.0)
+	{
+
+	}
+
+	public toString()
+	{
+		return 'rgba(' + this.r + ',' + this.g + ','
+		this.b + ',' + this.a + ')';
+	}
 }
 
 export = RGBA;
