82f2917b0b03a8faaa8d28511c25a2fda4a6442c
Mient-jan Stelling <mientjan.stelling@gmail.com>
2015-10-07 20:24:27 +0200

    working at seperation of ui interaction

--- examples/image-drawing/flump-animation.js
+++ examples/image-drawing/flump-animation.js
@@ -1,9 +1,7 @@
 define(["require", "exports", '../../src/easelts/display/Stage', '../../src/easelts/animation/FlumpLibrary', "../../src/easelts/util/ArrayUtil"], function (require, exports, Stage_1, FlumpLibrary_1, ArrayUtil_1) {
     var holder = document.getElementById('holder');
     var stage = new Stage_1.default(holder, true).setFpsCounter(true);
-    stage.ctx.canvas.style.transform = 'translate3d(0,0,-1px) scale(1.000001);';
-    stage.ctx.canvas.style.transformStyle = 'preserve-3d';
-    FlumpLibrary_1.default.load('../assets/flump/a-100-8x8l-1024/character').then(function (fl) {
+    FlumpLibrary_1.default.load('../assets/flump/animations-100/character').then(function (fl) {
         var names = [
             'SupermanSuduction1',
             'SupermanSuduction2',
@@ -24,6 +22,6 @@ define(["require", "exports", '../../src/easelts/display/Stage', '../../src/ease
         });
         stage.start();
         setTimeout(function () {
-        }, 5000);
+        }, 5000); 
     }).catch(function (error) { return console.log(error); });
 });
